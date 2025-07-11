f53b263080b57eabab25495b79720f66d854e031
Kaushal Bikram Gajmer <kaushalbikram44@gmail.com>
2025-07-11 15:15:31 +0545

    stable sanity setup

---
+++ BackendY/schemaTypes/blockContentType.ts
@@ -0,0 +1,25 @@
+import { defineField, defineType } from 'sanity';
+
+export const blockContentType = defineType({
+    name: 'blockContent',
+    title: 'Block Content',
+    type: 'array',
+    of: [
+        {
+            type: 'block',
+            styles: [
+                { title: 'Normal', value: 'normal' },
+                { title: 'H1', value: 'h1' },
+                { title: 'H2', value: 'h2' },
+                { title: 'H3', value: 'h3' },
+            ],
+            lists: [{ title: 'Bullet', value: 'bullet' }],
+            marks: {
+                decorators: [
+                    { title: 'Strong', value: 'strong' },
+                    { title: 'Emphasis', value: 'em' },
+                ],
+            },
+        },
+    ],
+});
---
+++ BackendY/schemaTypes/index.ts
@@ -0,0 +1,7 @@
+import { blockContentType } from "./blockContentType";
+import { categoryType } from "./categoryType";
+import { productType } from "./productType";
+import {orderType} from "./orderType";
+import {salesType} from "./salesType";
+
+export const schemaTypes = [blockContentType, categoryType, productType, orderType,salesType];
